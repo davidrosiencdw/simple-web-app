fc8fb56e827709a864862b5e6c07bf23a1f2eac6
davidrosiencdw <david.rosien@cdw.ca>
2023-07-05 13:55:39 -0500

    initial commit

---
+++ main1.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+# Create the Flask application object
+app = Flask(__name__)
+
+# Define a route and function to handle incoming requests
+@app.route('/')
+def hello_world():
+    return 'David Test123'
+
+# Start the web server
+if __name__ == '__main__':
+    app.run(host='localhost', port=5000)
